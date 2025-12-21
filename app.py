32a2aef17a059167a593d3305fcf588ee5d837a4
Vijayragavan <24ucs181@kamarajengg.edu.in>
2025-12-21 14:12:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import streamlit.components.v1 as components
 FILE_ID = "1mhkdGOadbGplRoA1Y-FTiS1yD9rVgcXB"
 MODEL_PATH = "driver_drowsiness.h5"
 CLASSES = ["notdrowsy", "drowsy"]
-ALERT_TIME = 2  # seconds before alarm triggers
+ALERT_TIME = 2  # seconds
 
 RTC_CONFIG = RTCConfiguration(
     {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
